b17800643edf77a83119b1d4cbc7b7296c6a72e5
cem256 <cemo99@gmail.com>
2024-06-18 16:23:05 +0300

    add logger #4 and navigator observer #5

---
+++ __brick__/lib/app/router/custom_route_observer.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: strict_raw_type
+
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:{{project_name.snakeCase()}}/core/utils/logger/logger_utils.dart';
+
+/// Custom route observer that logs all route changes
+final class CustomRouteObserver extends AutoRouteObserver {
+  @override
+  void didPush(Route route, Route? previousRoute) {
+    super.didPush(route, previousRoute);
+
+    LoggerUtils.instance.logInfo(
+      '[AutoRoute] New route pushed: ${route.settings.name}, Previous route: ${previousRoute?.settings.name}',
+    );
+  }
+
+  @override
+  void didPop(Route route, Route? previousRoute) {
+    super.didPop(route, previousRoute);
+    LoggerUtils.instance
+        .logInfo('[AutoRoute] Route popped: ${route.settings.name}, Previous route: ${previousRoute?.settings.name}');
+  }
+
+  @override
+  void didReplace({Route? newRoute, Route? oldRoute}) {
+    super.didReplace(newRoute: newRoute, oldRoute: oldRoute);
+    LoggerUtils.instance
+        .logInfo('[AutoRoute] Route replaced: ${newRoute?.settings.name}, Old route: ${oldRoute?.settings.name}');
+  }
+
+  @override
+  void didRemove(Route route, Route? previousRoute) {
+    super.didRemove(route, previousRoute);
+    LoggerUtils.instance
+        .logInfo('[AutoRoute] Route removed: ${route.settings.name}, Previous route: ${previousRoute?.settings.name}');
+  }
+
+  @override
+  void didInitTabRoute(TabPageRoute route, TabPageRoute? previousRoute) {
+    LoggerUtils.instance
+        .logInfo('[AutoRoute] Tab route initialized: ${route.name}, Previous tab route: ${previousRoute?.name}');
+  }
+
+  @override
+  void didChangeTabRoute(TabPageRoute route, TabPageRoute previousRoute) {
+    LoggerUtils.instance
+        .logInfo('[AutoRoute] Tab route changed: ${route.name}, Previous tab route: ${previousRoute.name}');
+  }
+}
