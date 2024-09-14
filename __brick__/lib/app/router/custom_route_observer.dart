624a353e746aa8169719bca63c2b6095c5a07e3f
cem256 <cemo99@gmail.com>
2024-09-14 13:13:14 +0300

    add logging config for CustomRouteObserver #10

--- __brick__/lib/app/router/custom_route_observer.dart
+++ __brick__/lib/app/router/custom_route_observer.dart
@@ -6,45 +6,78 @@ import 'package:{{project_name.snakeCase()}}/core/utils/logger/logger_utils.dart
 
 /// Custom route observer that logs all route changes
 final class CustomRouteObserver extends AutoRouteObserver {
+  CustomRouteObserver({
+    this.logPush = true,
+    this.logPop = true,
+    this.logReplace = true,
+    this.logRemove = true,
+    this.logInitTab = true,
+    this.logChangeTab = true,
+  });
+  final bool logPush;
+  final bool logPop;
+  final bool logReplace;
+  final bool logRemove;
+  final bool logInitTab;
+  final bool logChangeTab;
+
   @override
   void didPush(Route route, Route? previousRoute) {
     super.didPush(route, previousRoute);
-
-    LoggerUtils.instance.logInfo(
-      '[AutoRoute] New route pushed: ${route.settings.name}, Previous route: ${previousRoute?.settings.name}',
-    );
+    if (logPush) {
+      LoggerUtils.instance.logInfo(
+        '[AutoRoute] New route pushed: ${route.settings.name}, Previous route: ${previousRoute?.settings.name}',
+      );
+    }
   }
 
   @override
   void didPop(Route route, Route? previousRoute) {
     super.didPop(route, previousRoute);
-    LoggerUtils.instance
-        .logInfo('[AutoRoute] Route popped: ${route.settings.name}, Previous route: ${previousRoute?.settings.name}');
+    if (logPop) {
+      LoggerUtils.instance.logInfo(
+        '[AutoRoute] Route popped: ${route.settings.name}, Previous route: ${previousRoute?.settings.name}',
+      );
+    }
   }
 
   @override
   void didReplace({Route? newRoute, Route? oldRoute}) {
     super.didReplace(newRoute: newRoute, oldRoute: oldRoute);
-    LoggerUtils.instance
-        .logInfo('[AutoRoute] Route replaced: ${newRoute?.settings.name}, Old route: ${oldRoute?.settings.name}');
+    if (logReplace) {
+      LoggerUtils.instance.logInfo(
+        '[AutoRoute] Route replaced: ${newRoute?.settings.name}, Old route: ${oldRoute?.settings.name}',
+      );
+    }
   }
 
   @override
   void didRemove(Route route, Route? previousRoute) {
     super.didRemove(route, previousRoute);
-    LoggerUtils.instance
-        .logInfo('[AutoRoute] Route removed: ${route.settings.name}, Previous route: ${previousRoute?.settings.name}');
+    if (logRemove) {
+      LoggerUtils.instance.logInfo(
+        '[AutoRoute] Route removed: ${route.settings.name}, Previous route: ${previousRoute?.settings.name}',
+      );
+    }
   }
 
   @override
   void didInitTabRoute(TabPageRoute route, TabPageRoute? previousRoute) {
-    LoggerUtils.instance
-        .logInfo('[AutoRoute] Tab route initialized: ${route.name}, Previous tab route: ${previousRoute?.name}');
+    super.didInitTabRoute(route, previousRoute);
+    if (logInitTab) {
+      LoggerUtils.instance.logInfo(
+        '[AutoRoute] Tab route initialized: ${route.name}, Previous tab route: ${previousRoute?.name}',
+      );
+    }
   }
 
   @override
   void didChangeTabRoute(TabPageRoute route, TabPageRoute previousRoute) {
-    LoggerUtils.instance
-        .logInfo('[AutoRoute] Tab route changed: ${route.name}, Previous tab route: ${previousRoute.name}');
+    super.didChangeTabRoute(route, previousRoute);
+    if (logChangeTab) {
+      LoggerUtils.instance.logInfo(
+        '[AutoRoute] Tab route changed: ${route.name}, Previous tab route: ${previousRoute.name}',
+      );
+    }
   }
 }
